0ffe53f7e3ae6f5b63fff7971b6a36b7eebbcf85
Your Name <you@example.com>
2018-05-22 12:46:56 -0500

    inicio modulo admin

--- app/Jornada.php
+++ app/Jornada.php
@@ -3,8 +3,18 @@
 namespace Miomo;
 
 use Illuminate\Database\Eloquent\Model;
+use Miomo\Jornada;
+use Miomo\Partido;
 
 class Jornada extends Model
 {
     //
+
+    public function index(){
+
+      $jornada = Jornada::where('id_jornada','=', $id)->get();
+      $partidos = Partido::where('id_jornada','=',$id)->get();
+
+      $
+    }
 }
