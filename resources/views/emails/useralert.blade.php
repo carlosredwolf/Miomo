cf3409ac6126c057e9042a60361366690bc05415
celedonio2296 <cesar.celedonio@red-wolf.com.mx>
2018-05-23 12:50:02 -0500

    Diseño mensaje

--- resources/views/emails/useralert.blade.php
+++ resources/views/emails/useralert.blade.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="utf-8">
-</head>
-<body>
+@extends('layouts.apph')
+@section('content')
     <h1>Se ha enviado un correo de confirmación, por favor revisa tu cuenta de correo.</h1>
-</body>
-</html>
+@endsection
