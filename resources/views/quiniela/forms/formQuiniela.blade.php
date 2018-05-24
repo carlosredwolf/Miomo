5b7617a856e31c6e380515cc00eae68d1bbf8931
Your Name <you@example.com>
2018-05-24 17:04:24 -0500

    mis quinielas

--- resources/views/quiniela/forms/formQuiniela.blade.php
+++ resources/views/quiniela/forms/formQuiniela.blade.php
@@ -19,14 +19,27 @@
       @endphp
       <div>
         <label class="eEquipo1" for="radio-{{$partido->id}}"><img src="{{asset($route1)}}"> <span>{{$partido->local->nombre}}</span></label>
-        <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=1>
+        @if ($partido->resultado->id == 1)
+            <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=1 checked>
+        @else
+            <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=1>
+        @endif
+
       </div>
       <div class ="deEmpate">
-        <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=2>
+        @if ($partido->resultado->id == 2)
+          <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=2 checked>
+        @else
+          <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=2>
+        @endif
         <label  class ="eEmpate" for="radio-{{$partido->id}}">Empate</label>
       </div>
       <div>
-        <input  class="radio square" type="radio" name="radio-{{$partido->id}}" value=3>
+        @if ($partido->resultado->id == 3)
+          <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=3 checked>
+        @else
+          <input class="radio square" type="radio" name="radio-{{$partido->id}}" value=3>
+        @endif
         <label class="eEquipo2" for="radio-{{$partido->id}}"><img src="{{asset($route2)}}" >	<span class="nomEquipo2">{{$partido->visitante->nombre}}</span></label>
       </div>
     </div>
