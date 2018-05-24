4909093f2fe11dcb3741a6eb8884ba00c4b1f006
Your Name <you@example.com>
2018-05-23 21:06:43 -0500

    se agrega el env

--- config/database.php
+++ config/database.php
@@ -1,16 +1,11 @@
 <?php
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//
-// $host = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $database = substr($url["path"], 1);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = 'localhost';
-$username ='root';
-$password ='';
-$database ='miomo';
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
 
